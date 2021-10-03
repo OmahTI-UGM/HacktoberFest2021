94c36419a95c3ac5584276d6d46b1f2cae65fe8a
Jokushu <gnwnvincent@gmail.com>
2021-10-03 20:20:37 +0700

    Project 1

---
+++ project1/Project1/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by TwoStraws on 12/08/2016.
+//  Copyright © 2016 Paul Hudson. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+	@IBOutlet var imageView: UIImageView!
+	var selectedImage: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+
+		if let imageToLoad = selectedImage {
+			imageView.image  = UIImage(named: imageToLoad)
+		}
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+}
